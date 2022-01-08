f78c01115f88b0c195a95181dc83eff6716ceec0
userName1 <hello@gmail.com>
2022-01-08 19:17:11 +0100

    JSON response

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
+const products = require('./data/products.json');
 
+// Because we don not specific any method which server can be listen..
+// We are getting those products with all http methods (GET, POST,..)
 const server = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write('<h1>Hello world</h1>')
-    res.end();
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(products)); 
+    // instead res.write(...); res.end(); => res.end(...)
 });
 
 const PORT = process.env.PORT || 5000;
